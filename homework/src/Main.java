8acda795647c8e2ef6e44327054df6f722ce85b6
4708696-UniGe <S4708696@studenti.unige.it>
2020-11-17 10:44:54 +0100

    add args requirement

---
+++ homework/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		if (args.length != 3) {	// Require 3 arguments otherwise exit
+			 System.exit(0);	//
+		 }						//
+		
+		
+	}
+
+}
